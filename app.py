701b175f1b557a205245e98607c4c487e2868f02
Starling7 <129472418+Starling7@users.noreply.github.com>
2023-12-03 20:17:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,71 @@
 import streamlit as st
 from sqlalchemy import text
 
-list_product = ['', 'Sepatu', 'Bola', 'Jersey', 'Topi', 'Celana']
-list_ongkir = ['', 'FREE', 'FEE']
+list_class = ['', 'Economy', 'VIP', 'VVIP', 'Reguler',]
+list_gender = ['', 'male', 'female']
 
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://radityacr740:o8KrhDcWj4wN@ep-super-smoke-81752083.us-east-2.aws.neon.tech/fpmbddb")
 with conn.session as session:
-    query = text('CREATE TABLE IF NOT EXISTS LOGISTICS (id serial, product_nama varchar, ekspedisi_nama varchar, ongkir text, kota_tujuan text, handphone varchar, nomor_resi text, tanggal_dikirim date, tanggal_sampai);')
+    query = text('CREATE TABLE IF NOT EXISTS TICKETS (id serial, class_name varchar, supporter_name varchar, gender char(25), \
+                                                       stadium_name varchar, ticket_price varchar, match_name text, time_info time, date_info date);')
     session.execute(query)
 
-st.header('DATA LOGISTIK PT SIGMA JAYA')
+st.header('FOOTBALL TICKETS')
 page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
 
 if page == "View Data":
-    data = conn.query('SELECT * FROM logistics ORDER By id;', ttl="0").set_index('id')
+    data = conn.query('SELECT * FROM tickets ORDER By id;', ttl="0").set_index('id')
     st.dataframe(data)
 
 if page == "Edit Data":
     if st.button('Tambah Data'):
         with conn.session as session:
-            query = text('INSERT INTO logistics (product_nama, ekspedisi_nama, ongkir, kota_tujuan, handphone, nomor_resi, tanggal_dikirim, tanggal_sampai) \
-                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9);')
-            session.execute(query, {'1':'', '2':'', '3':'', '4':'', '5':'', '6':'', '7':None, '8':None})
+            query = text('INSERT INTO tickets (class_name, supporter_name, gender, stadium_name, ticket_price, match_name, time_info, date_info) \
+                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8);')
+            session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':'', '6':'', '7':None, '8':None})
             session.commit()
 
-    data = conn.query('SELECT * FROM logistics ORDER By id;', ttl="0")
+    data = conn.query('SELECT * FROM tickets ORDER By id;', ttl="0")
     for _, result in data.iterrows():        
         id = result['id']
-        product_nama_lama = result["product_nama"]
-        ekspedisi_nama_lama = result["ekspedisi_nama"]
-        ongkir_lama = result["ongkir"]
-        kota_tujuan_lama = result["kota_tujuan"]
-        handphone_lama = result["handphone"]
-        nomor_resi_lama = result["nomor_resi"]
-        tanggal_dikirim_lama = result["tanggal_dikirim"]
-        tanggal_sampai_lama = result["tanggal_sampai"]
+        class_name_lama = result["class_name"]
+        supporter_name_lama = result["supporter_name"]
+        gender_lama = result["gender"]
+        stadium_name_lama = result["stadium_name"]
+        ticket_price_lama = result["ticket_price"]
+        match_name_lama = result["match_name"]
+        time_info_lama = result["time_info"]
+        date_info_lama = result["date_info"]
 
-        with st.expander(f'a.n. {product_nama_lama}'):
+        with st.expander(f'a.n. {supporter_name_lama}'):
             with st.form(f'data-{id}'):
-                product_nama_baru = st.selectbox("product_nama", list_product, list_product.index(product_nama_lama))
-                ekspedisi_nama_baru = st.text_input("ekspedisi_nama", ekspedisi_nama_lama)
-                ongkir_baru = st.selectbox("ongkir", list_ongkir, list_ongkir.index(ongkir_lama))
-                kota_tujuan_baru = st.text_input("kota_tujuan" , kota_tujuan_lama)
-                handphone_baru = st.text_input("handphone", handphone_lama)
-                nomor_resi_baru = st.text_input("nomor_resi", nomor_resi_lama)
-                tanggal_dikirim_baru = st.date_input("tanggal_dikirim", tanggal_dikirim_lama)
-                tanggal_sampai_baru = st.date_input("tanggal_sampai", tanggal_sampai_lama)
+                class_name_baru = st.selectbox("class_name", list_class, list_class.index(class_name_lama))
+                supporter_name_baru = st.text_input("supporter_name", supporter_name_lama)
+                gender_baru = st.selectbox("gender", list_gender, list_gender.index(gender_lama))
+                stadium_name_baru = st.text_input("stadium_name", departure_station_lama)
+                ticket_price_baru = st.text_input("ticket_price", ticket_price_lama)
+                match_name_baru = st.text_input("match_name", match_name_lama)
+                time_info_baru = st.time_input("time_info", time_info_lama)
+                date_info_baru = st.date_input("date_info", date_info_lama)
                 
                 col1, col2 = st.columns([1, 6])
 
                 with col1:
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
-                            query = text('UPDATE logistics \
-                                          SET product_nama=:1, ekspedisi_nama=:2, ongkir=:3, kota_tujuan=:4, \
-                                          handphone=:5, nomor_resi=:6, tanggal_dikirim=:7, tanggal_sampai=:8 \
+                            query = text('UPDATE tickets \
+                                          SET class_name=:1, supporter_name=:2, gender=:3, stadium_name=:4, \
+                                          ticket_price=:5, match_name=:6, time_info=:7, date_info=:8 \
                                           WHERE id=:9;')
-                            session.execute(query, {'1':product_nama_baru, '2':ekspedisi_nama_baru, '3':ongkir_baru, '4':kota_tujuan_baru, 
-                                                    '5':handphone_baru, '6':nomor_resi_baru, '7':tanggal_dikirim_baru, '8':tanggal_sampai_baru, '9':id})
+                            session.execute(query, {'1':class_name_baru, '2':supporter_name_baru, '3':gender_baru, '4':str(stadium_name), 
+                                                    '5':ticket_price_baru, '6':match_name_baru, '7':time_info_baru, '8':date_info_baru, '9':id})
                             session.commit()
                             st.experimental_rerun()
                 
                 with col2:
                     if st.form_submit_button('DELETE'):
-                        query = text(f'DELETE FROM schedule WHERE id=:1;')
+                        query = text(f'DELETE FROM tickets WHERE id=:1;')
                         session.execute(query, {'1':id})
                         session.commit()
                         st.experimental_rerun()
