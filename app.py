e28d7069923a6a85bfd38557cb99c3415d764c1d
Starling7 <129472418+Starling7@users.noreply.github.com>
2023-12-15 00:44:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from sqlalchemy import text
 list_train = ['', 'Economy', 'Regular', 'VIP', 'VVIP']
 list_gender = ['', 'male', 'female']
 list_match= ['', 'Indonesia vs Argentina', 'Indonesia vs Thailand', 'Indonesia vs Malaysia']
+list_stadium = ['', 'Jakarta International Stadium', 'Jatidiri Stadium', 'Maguwoharjo Stadium']
+list_price = ['', '150000', '250000', '350000', '500000']
 
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://radityacr740:o8KrhDcWj4wN@ep-super-smoke-81752083.us-east-2.aws.neon.tech/fpmbddb")
@@ -43,8 +45,8 @@ if page == "Edit Data":
                 tribune_name_baru = st.selectbox("tribune_name", list_train, list_train.index(tribune_name_lama))
                 supporter_name_baru = st.text_input("supporter_name", supporter_name_lama)
                 gender_baru = st.selectbox("gender", list_gender, list_gender.index(gender_lama))
-                stadium_name_baru = st.text_input("stadium_name", stadium_name_lama)
-                ticket_price_baru = st.text_input("ticket_price", ticket_price_lama)
+                stadium_name_baru = st.selectbox("stadium_name", list_stadium, list_stadium.index(stadium_name_lama))
+                ticket_price_baru = st.selectbox("ticket_price", list_ticket, list_ticket.index(ticket_price_lama))
                 match_name_baru = st.selectbox("match_name", list_match, list_match.index(match_name_lama))
                 date_info_baru = st.date_input("date_info", date_info_lama)
                 
