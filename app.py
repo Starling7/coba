b3aef624f73fa0ca9e3df60e51b640b0dfbc7de9
Starling7 <129472418+Starling7@users.noreply.github.com>
2023-12-15 00:38:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if page == "Edit Data":
                 gender_baru = st.selectbox("gender", list_gender, list_gender.index(gender_lama))
                 stadium_name_baru = st.text_input("stadium_name", stadium_name_lama)
                 ticket_price_baru = st.text_input("ticket_price", ticket_price_lama)
-                match_name_baru = st.selectbox("match_name", list_match.index(match_name_lama))
+                match_name_baru = st.selectbox("match_name", list_match, list_match.index(match_name_lama))
                 date_info_baru = st.date_input("date_info", date_info_lama)
                 
                 col1, col2 = st.columns([1, 6])
